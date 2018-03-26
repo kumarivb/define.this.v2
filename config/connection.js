47a5bf5fe27cd3f28358199acbc4de718445b86e
kvb <kumarivb@gmail.com>
2018-03-26 16:00:09 -0500

    pass

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "root",
+    password: "asdf",
     database: "definethis_db"
 });
 }
